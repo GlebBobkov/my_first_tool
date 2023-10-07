4cfc9f0b9c4051c56c63a9580fcf6a7ef9dc16ab
Gleb Bobkov <gleb@LAPTOP-TTFELUK1>
2023-10-07 14:49:28 +0300

    add rename_three_letter_name function

--- moduls/protein_tools.py
+++ moduls/protein_tools.py
@@ -1,10 +1,30 @@
- operations = {
+operations = {
         'length': length_info,
         'percentage': count_percentage_aa,
         'pattern': find_pattern,
         '3Letter_name': rename_three_letter_name,
         'DNA_code': get_protein_gene
     }
+
+def rename_three_letter_name(seqs: list, sep='') -> list:
+    """
+    Transform into a three-letter amino acids entry.
+    arguments:
+        - seqs (list): list of sequences for transforming to three-letter entire
+        - sep (str): separator between aminoacids, default = ''
+    return:
+        - list: transformed sequences with separators
+    """
+    res = []
+    for seq in seqs:
+        threel_form = ''
+        for aa in seq:
+            threel_form = threel_form + threel[aa] + sep
+        if sep:
+            threel_form = threel_form[:-1]
+        res.append(threel_form)
+    return res
+
 
 def protein_tool(*proteins, options=None):
     proteins = list(proteins)

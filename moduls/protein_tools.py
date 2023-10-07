e820304773def7e788ce13b95ad29bca9772474d
Gleb Bobkov <gleb@LAPTOP-TTFELUK1>
2023-10-07 14:42:30 +0300

    add protein_tool function

---
+++ moduls/protein_tools.py
@@ -0,0 +1,26 @@
+def protein_tool(*proteins, options=None):
+    proteins = list(proteins)
+
+    operations = {
+        'compare': compare,
+        'length': length_info,
+        'percentage': count_percentage_aa,
+        'pattern': find_pattern,
+        '3Letter_name': rename_three_letter_name,
+        'DNA_code': get_protein_gene
+    }
+
+    if options == 'compare':
+        result = operations[options](proteins[:-2], proteins[-2], proteins[-1])
+        return (result)
+    elif options == '3Letter_name':
+        result = operations[options](proteins[:-1], proteins[-1])
+        return result
+    elif options == 'length' or options == 'percentage' or options == 'DNA_code':
+        result = []
+        for protein in proteins:
+            res = operations[options](protein)
+            result.append(res)
+        return (result)
+    else:
+        raise ValueError('Incorrect options input, please try again')

f76447976716cec53f30a461af6f22fc432e61a5
Gleb Bobkov <gleb@LAPTOP-TTFELUK1>
2023-10-07 03:19:43 +0300

    add dna_rna_tool funtion to the dna_rna_tools.py

---
+++ dna_rna_tools.py
@@ -0,0 +1,12 @@
+def dna_rna_tools (*args):
+    *sequnses, def_name = args
+    for sequns in sequnses:
+        if RNA_checking(sequns) == True:
+            continue
+        else:
+            raise ValueError('Incorrect options input, please try again')
+    result = []
+    for sequns in sequnses:
+        res = operations[def_name](sequns)
+        result.append(res)
+    return (result) 

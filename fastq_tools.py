9f56034f5be285ee7a3d78a51ac94c21bf882529
Gleb Bobkov <gleb@LAPTOP-TTFELUK1>
2023-10-07 03:07:54 +0300

    add GC_cont_check to fastaq_tool.py

--- fastq_tools.py
+++ fastq_tools.py
@@ -1,3 +1,23 @@
+def GC_cont_check(sequence_for_filtering: str , gc_bounds: list) -> bool:
+    """
+    Check the GC content of the sequence for filtering
+    :param sequence_for_filtering: analyzed sequence
+    :param gc_bounds: list with limitation for GC content
+    :return: bool argument for filtering in fasta_filtering function
+    """
+    GC_counter = 0
+    for i in sequence_for_filtering:
+        if i == "C" or i == "G" or i == "c" or i == "g":
+            GC_counter += 1
+    GC_calculc = GC_counter/len(sequence_for_filtering)*100
+    if len(gc_bounds)==1:
+        if GC_calculc <= gc_bounds[0]:
+            return True
+    if len(gc_bounds) == 2:
+        if GC_calculc <= gc_bounds[1] and GC_calculc >= gc_bounds[0]:
+            return True
+
+
 def fasta_filtering(seqs, gc_bounds = (0, 100), length_bounds = [0, 2**32], quality_threshold = 0):
     outline_new_dict_fasta = {}
     inline_new_dit_fasta = {}

75d88c672061e9a1fe1b07d7c3fb17d4c906f938
Gleb Bobkov <gleb@LAPTOP-TTFELUK1>
2023-10-07 03:22:31 +0300

    add dictionary for transription and complemetarity functions

--- dna_rna_tools.py
+++ dna_rna_tools.py
@@ -5,6 +5,22 @@ operations = {
         'reverse_complement': reverse_complement
     }
 
+transcription_dict = {
+    'A': 'U', 'a': 'u',
+    'T': 'A', 't': 'a',
+    'G': 'C', 'g': 'c',
+    'C': 'g', 'c': 'g'
+}
+
+
+complement_dict = {
+    'A': 'T', 'a': 't',
+    'T': 'A', 't': 'a',
+    'G': 'C', 'g': 'c',
+    'C': 'g', 'c': 'g'
+}
+
+
 def dna_rna_tools (*args):
     *sequnses, def_name = args
     for sequns in sequnses:
